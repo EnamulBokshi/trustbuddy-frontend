e75e924ac026d297915a0607b33cbd888ef5dc5f
Enamul Haque <enamulbokshi@gmail.com>
2024-12-01 16:54:20 +0600

    migrated from mehedi1952

---
+++ src/components/PriceCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PriceCard() {
+  return (
+    <div className='bg-gray-600 p-5 rounded-lg'>
+        <h2>Free</h2>
+        <h1 className='text-4xl font-bold '>$0.0</h1>
+    </div>
+  )
+}
+
+export default PriceCard
--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import Footer from '../components/Footer'
 function Pricing() {
   return (
     <>
       <Nav active='pricing'/>
+
+      <Footer />
     </>
   )
 }
